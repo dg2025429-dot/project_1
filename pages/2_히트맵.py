41a7b0d6c4224ebcdee91355d1b6a2d8dcd8d6f1
dg2025429-dot <dg2025429@danggok.hs.kr>
2026-06-17 09:58:17 +0900

    Update 2_히트맵.py

--- pages/2_히트맵.py
+++ pages/2_히트맵.py
@@ -2,16 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-@st.cache_data
-def load_data():
-    return pd.read_csv(
-        "data/서울교통공사_지하철혼잡도정보_20260331.csv",
-        encoding="cp949"
-    )
-
-df = load_data()
+df = pd.read_csv(
+    "서울교통공사_지하철혼잡도정보_20260331.csv",
+    encoding="cp949"
+)
 
 time_cols = df.columns[5:]
+
+for col in time_cols:
+    df[col] = pd.to_numeric(
+        df[col],
+        errors="coerce"
+    )
 
 st.title("🔥 혼잡도 히트맵")
 
@@ -20,27 +22,17 @@ line = st.selectbox(
     sorted(df["호선"].unique())
 )
 
-subset = df[
-    (df["호선"]==line) &
-    (df["요일구분"]=="평일")
-]
-
 heat_df = (
-    subset.groupby("출발역")[time_cols]
+    df[df["호선"]==line]
+    .groupby("출발역")[time_cols]
     .mean()
 )
 
 fig = px.imshow(
     heat_df,
     aspect="auto",
-    labels=dict(
-        x="시간",
-        y="역",
-        color="혼잡도"
-    )
+    color_continuous_scale="Reds"
 )
-
-fig.update_layout(height=900)
 
 st.plotly_chart(
     fig,
